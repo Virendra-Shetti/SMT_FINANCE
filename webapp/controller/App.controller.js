81a5dcb207506479629f9dccc8ab8dc11a89eec4
Virendra Korishetti <codingvirendra@gmail.com>
2020-08-17 11:34:31 +0000

    First commit By Virendra

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("SF.SMT_FINANCE.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
