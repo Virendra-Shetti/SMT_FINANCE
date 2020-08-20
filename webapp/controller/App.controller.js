63c3ef4f3400c829c7976a8531061e0de689c38a
Abhilasha Appaji <abhiappaji1991@gmail.com>
2020-08-20 07:51:43 +0000

    C_Abhilasha_mockserver

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("SF.SMT_FINANCE.controller.App", {
@@ -30,7 +32,28 @@ sap.ui.define([
 			} else {
 				oToggleButton.setTooltip('Small Size Navigation');
 			}
+		},
+	
+	/////////////////////Functionality for Combo Boxes in Salary management/////////////
+	onSelectionChange:function(){
+		debugger;
+		var oModel1 = this.getView().getModel().getData().EmployeeDetails;
+		var comboxDept = this.getView().byId("ComboBox1").getValue();
+		for(var i=0; i<oModel1.length ; i++){
+			if(comboxDept === oModel1[i].Department){
+			var filterDeptName = new Filter ("comboxDept", FilterOperator.EQ, "SAP UI5");
+			var oCombox2 = this.getView().byId("ComboBox2");
+			oCombox2.getBinding("items").filter(filterDeptName);
+			}
 		}
-		//////////////////////////////////////////////////////////////////////////////////
+	}
+	// onPressItem:function(oEvent){
+	// debugger;	
+	// var oModel2 = this.getView().getModel().getData().EmployeeDetails;
+	// var comboxEmpName = this.getView().byId("ComboBox2").getValue();
+	// for(var j=0; j<oModel2.length; j++){
+	// 	if 
+	// }
+	// }
 	});
 });
