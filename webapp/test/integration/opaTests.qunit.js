d85167f32bae9a74c756a00fb5088c7e5c1468c4
Abhilasha Appaji <abhiappaji1991@gmail.com>
2020-09-04 07:29:10 +0000

    C_Abhilajsha_04_09_2020

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"SF/SMT_FINANCE/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
